6bcf4459a4c8d5d98ddf5d7b160288c764726296
Andrew Simmons <neon.duncan@gmail.com>
2020-12-09 14:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (creditNumber === undefined) {
-    console.log("Sorry, we don't have a credit card on file for you.")
+    return "Sorry, we don't have a credit card on file for you."
   } 
   cart.splice(0, cart.length - 1)
   return `Your total cost is $${total()}, which will be charged to the card ${creditNumber}.`
