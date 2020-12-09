cdc1b72d10d8358a3c0abf56fda52624481ee1d6
Andrew Simmons <neon.duncan@gmail.com>
2020-12-09 14:53:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,12 +55,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (typeof(creditNumber) === 'undefined') {
-    console.log(typeof(creditNumber))
+  if (typeof(cardNumber) === 'undefined') {
+    console.log(typeof(cardNumber))
     return "Sorry, we don't have a credit card on file for you."
   } 
   console.log(cart.splice(0, cart.length))
-  return `Your total cost is $${total()}, which will be charged to the card ${creditNumber}.`
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
 
 console.log(placeOrder(83296759))
