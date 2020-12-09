02da1ab365d626a669fcb0e06165b1bc4a6746b4
Andrew Simmons <neon.duncan@gmail.com>
2020-12-09 14:58:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function placeOrder(cardNumber) {
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
 
-console.log(placeOrder())
+console.log(placeOrder(11111))
 console.log(cart)
 
 
