1533a0ef54292a17a15ccb6191b34ee2f5d3a793
Andrew Simmons <neon.duncan@gmail.com>
2020-12-09 13:11:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,21 +8,44 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+  
 function addToCart(item) {
- // write your code here
+ cart.push({itemName: item, itemPrice: getRandomInt(101)})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  //I don't think this if statemet will work according to the lst one
+  if (cart[0] === undefined) {
+    return "Your shopping cart is empty."
+  } else {
+    let message = "In your cart, you have"
+    cart.forEach(function(item, index) {
+      if (cart.length === 1) {
+        message += ` ${cart[index].itemName} at $${cart[index].itemPrice}.`
+      } else if (index === cart.length - 1) {
+        message += ` and ${cart[index].itemName} at $${cart[index].itemPrice}.`
+      } else {
+        message += ` ${cart[index].itemName} at $${cart[index].itemPrice},`
+      }
+    })
+    return message
+  }
 }
 
 function total() {
-  // write your code here
+  let total = 0
+  cart.forEach(function(item, index) {
+    total += cart[index].itemPrice
+  })
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  
 }
 
 function placeOrder(cardNumber) {
